7399c828c29acd5a587bcbce84f7d3648c6638fc
ahmedelsayedsaleh <ahmed0elsayed0saleh@gmail.com>
2022-06-03 00:58:09 +0200

    Get currently weather and display it in html

--- content/js/main.js
+++ content/js/main.js
@@ -22,10 +22,27 @@ const toggleTemp = () => {
     //   // temp.innerHTML = `${Math.round((temp.innerHTML - 32) * 5 / 9)}&deg;C`;
     // }
   });
-}; // end of toggleTemp
+};
 toggleTemp();
 
-// Get data from api
+// get current date
+const getDate = () => {
+  const date = new Date().toLocaleDateString("en-US", {
+    weekday: "long",
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  });
+  return date;
+};
+// display date in html
+const setDate = () => {
+  const date = document.querySelector("#date");
+  date.innerHTML = getDate();
+};
+setDate();
+
+// Get weather from api
 const url =
   "https://api.darksky.net/forecast/a177f8481c31fa96c3f95ad4f4f84610/30.007414,31.491318";
 
@@ -39,31 +56,12 @@ async function getData() {
     //   daily: { data: dailyData },
     // } = data;
     return data;
-    // console.log(data);
-    // return data;
   } catch (err) {
     console.log(err);
   }
-} // end of getData
+}
 
-// get current date
-const getDate = () => {
-  const date = new Date().toLocaleDateString("en-US", {
-    weekday: "long",
-    month: "short",
-    day: "numeric",
-    year: "numeric",
-  });
-  console.log(date);
-  return date;
-};
-// set date in html
-const setDate = () => {
-  const date = document.querySelector("#date");
-  date.innerHTML = getDate();
-};
-setDate();
-// set data html
+// display weather in html
 // const htmlDisplay = (data) => {
 // const {
 //   currently: { temperature, summary, icon },
@@ -73,22 +71,76 @@ setDate();
 // };
 
 getData().then((data) => {
-  // console.log(data);
-  const { temperature, summary, icon } = data.currently;
-  // Set DOM elements from the API
-  // temperatureDegree.textContent = temperature;
-  // temperatureDescription.textContent = summary;
-  // locationTimezone.textContent = data.timezone;
-  // // Set icon
-  // setIcons(icon, document.querySelector(".icon"));
-  // // Change temperature to Celsius/Fahrenheit
-  // temperatureSection.addEventListener("click", () => {
-  //   if (temperatureSpan.textContent === "F") {
-  //     temperatureSpan.textContent = "C";
-  //     temperatureDegree.textContent = Math.floor((temperature - 32) * (5 / 9));
-  //   } else {
-  //     temperatureSpan.textContent = "F";
-  //     temperatureDegree.textContent = temperature;
-  //   }
-  // });
+  console.log(data);
+  const { currently, daily, hourly } = data;
+  const { temperature, summary, icon } = currently;
+  const { data: dailyData } = daily;
+  const { data: hourlyData } = hourly;
+
+  const temp = document.querySelector("#temp");
+  temp.innerHTML = `${Math.round(
+    temperature
+  )}<span class="temperature-degree">&#176;</span>`;
+
+  const iconImg = document.querySelector("#icon");
+  iconImg.src = `https://darksky.net/images/weather-icons/${icon}.png`;
+
+  const summaryEl = document.querySelector("#summary");
+  summaryEl.innerHTML = summary;
+
+  const hourlySummary = document.querySelector("#hourly-summary");
+  hourlySummary.innerHTML = hourly.summary;
+
+  const hourlyEl = document.querySelector("#hourly");
+  hourlyEl.innerHTML = hourlyData.map((hour) => {
+    const { time, icon, temperature } = hour;
+    return `
+    <div class="hourly-item">
+      <div class="hourly-item__time">${time}</div>
+      <div class="hourly-item__icon">${icon}</div>
+      <div class="hourly-item__temp">${Math.round(temperature)}&deg;C</div>
+    </div>
+    `;
+  });
+
+  const dailyEl = document.querySelector("#daily");
+  dailyEl.innerHTML = dailyData.map((day) => {
+    const { time, icon, temperatureHigh, temperatureLow } = day;
+    return `
+    <div class="daily-item">
+      <div class="daily-item__time">${time}</div>
+      <div class="daily-item__icon">${icon}</div>
+      <div class="daily-item__temp">${Math.round(temperatureHigh)}&deg;C</div>
+      <div class="daily-item__temp">${Math.round(temperatureLow)}&deg;C</div>
+    </div>
+    `;
+  });
 });
+
+// const { temperature, summary, icon } = data.hourly;
+// const temp = document.querySelector("#temp");
+// temp.innerHTML = `${Math.round(temperature)}&deg;`;
+
+// const weather = document.querySelector("#weather");
+// weather.innerHTML = summary;
+
+// const iconImg = document.querySelector("#icon");
+// iconImg.src = `https://darksky.net/images/weather-icons/${icon}.png`;
+
+// Set DOM elements from the API
+// temperatureDegree.textContent = temperature;
+// temperatureDescription.textContent = summary;
+// locationTimezone.textContent = data.timezone;
+// // Set icon
+// setIcons(icon, document.querySelector(".icon"));
+// // Change temperature to Celsius/Fahrenheit
+// temperatureSection.addEventListener("click", () => {
+//   if (temperatureSpan.textContent === "F") {
+//     temperatureSpan.textContent = "C";
+//     temperatureDegree.textContent = Math.floor((temperature - 32) * (5 / 9));
+//   } else {
+//     temperatureSpan.textContent = "F";
+//     temperatureDegree.textContent = temperature;
+//   }
+// });
+// });
