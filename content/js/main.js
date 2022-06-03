41be7db0fe159833f8a384ee347d16d072750299
ahmedelsayedsaleh <ahmed0elsayed0saleh@gmail.com>
2022-06-03 23:11:24 +0200

    Edit some syntax error

--- content/js/main.js
+++ content/js/main.js
@@ -1,133 +1,136 @@
-// Celsius/Fahrenheit buttons toggle
-const toggleTemp = () => {
-  const tempBtn = document.querySelectorAll(".temperature-btn");
+window.addEventListener("load", () => {
+  // Celsius/Fahrenheit buttons toggle
+  const toggleTemp = () => {
+    const tempBtn = document.querySelectorAll(".temperature-btn");
 
-  tempBtn.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      tempBtn.forEach((btn) => {
-        btn.classListNaNpxove("temperature-btn--right-active");
-        btn.classListNaNpxove("temperature-btn--left-active");
+    tempBtn.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        tempBtn.forEach((btn) => {
+          btn.classList.remove("temperature-btn--right-active");
+          btn.classList.remove("temperature-btn--left-active");
+        });
+        if (btn.firstElementChild.innerHTML === "C") {
+          btn.classList.add("temperature-btn--left-active");
+        } else {
+          btn.classList.add("temperature-btn--right-active");
+        }
       });
-      if (btn.firstElementChild.innerHTML === "C") {
-        btn.classList.add("temperature-btn--left-active");
-      } else {
-        btn.classList.add("temperature-btn--right-active");
-      }
+      // if (tempBtnText.innerHTML === "C") {
+      //   tempBtnText.innerHTML = "F";
+      //   // temp.innerHTML = `${Math.round(temp.innerHTML * 9 / 5 + 32)}&deg;F`;
+      // } else {
+      //   tempBtnText.innerHTML = "C";
+      //   // temp.innerHTML = `${Math.round((temp.innerHTML - 32) * 5 / 9)}&deg;C`;
+      // }
     });
-    // if (tempBtnText.innerHTML === "C") {
-    //   tempBtnText.innerHTML = "F";
-    //   // temp.innerHTML = `${Math.round(temp.innerHTML * 9 / 5 + 32)}&deg;F`;
-    // } else {
-    //   tempBtnText.innerHTML = "C";
-    //   // temp.innerHTML = `${Math.round((temp.innerHTML - 32) * 5 / 9)}&deg;C`;
-    // }
-  });
-};
-toggleTemp();
+  };
+  toggleTemp();
 
-// get current date
-const getDate = () => {
-  const date = new Date().toLocaleDateString("en-US", {
-    weekday: "long",
-    month: "short",
-    day: "numeric",
-    year: "numeric",
-  });
-  return date;
-};
-// display date in html
-const setDate = () => {
-  const date = document.querySelector("#date");
-  date.innerHTML = getDate();
-};
-setDate();
+  // get current date
+  const getDate = () => {
+    const date = new Date().toLocaleDateString("en-US", {
+      weekday: "long",
+      month: "short",
+      day: "numeric",
+      year: "numeric",
+    });
+    return date;
+  };
+  // display date in html
+  const setDate = () => {
+    const date = document.querySelector("#date");
+    date.innerHTML = getDate();
+  };
+  setDate();
 
-// Get weather from api
-const url =
-  "https://api.darksky.net/forecast/a177f8481c31fa96c3f95ad4f4f84610/30.007414,31.491318";
+  // Get weather from api
+  const url =
+    "https://api.darksky.net/forecast/a177f8481c31fa96c3f95ad4f4f84610/30.007414,31.491318";
 
-async function getData() {
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    // const {
-    //   currently: { temperature, summary, icon },
-    //   hourly: { data: hourlyData },
-    //   daily: { data: dailyData },
-    // } = data;
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-// display weather in html
-getData().then((data) => {
-  console.log(data);
-  const { currently, daily, hourly } = data;
-  const { temperature, summary, icon } = currently;
-  const { data: hourlyData } = hourly;
-  const { data: dailyData } = daily;
-
-  // display current weather
-  const temp = document.querySelector("#temp");
-  temp.innerHTML = `${Math.round(
-    temperature
-  )}<span class="temperature-degree">&#176;</span>`;
-
-  const iconImg = document.querySelector("#icon");
-  iconImg.src = `https://darksky.net/images/weather-icons/${icon}.png`;
-
-  const summaryEl = document.querySelector("#summary");
-  summaryEl.innerHTML = summary;
-
-  const hourlySummary = document.querySelector("#hourly-summary");
-  hourlySummary.innerHTML = hourly.summary;
-
-  // display hourly weather
-  const hourlyEl = document.querySelector("#hourly");
-  hourlyData.forEach((hour) => {
-    const { time, icon, temperature } = hour;
-    const hourlyContainer = document.createElement("div");
-    hourlyContainer.classList.add(
-      "item",
-      "d-flex",
-      "flex-column",
-      "align-items-center"
-    );
-    const timeEl = document.createElement("p");
-    timeEl.classList.add("weather__forecasts__time");
-    if (new Date(time * 1000).getHours() === new Date().getHours()) {
-      timeEl.innerHTML = "Now";
-    } else {
-      timeEl.innerHTML = new Date(time * 1000).toLocaleTimeString("en-US", {
-        hour: "numeric",
-        minute: "numeric",
-        hour12: false,
-      });
+  async function getData() {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      // const {
+      //   currently: { temperature, summary, icon },
+      //   hourly: { data: hourlyData },
+      //   daily: { data: dailyData },
+      // } = data;
+      return data;
+    } catch (err) {
+      console.log(err);
     }
+  }
 
-    const iconEl = document.createElement("img");
-    iconEl.classList.add("weather__forecasts__img");
-    iconEl.src = `https://darksky.net/images/weather-icons/${icon}.png`;
+  // display weather in html
+  getData().then((data) => {
+    console.log(data);
+    const { currently, daily, hourly } = data;
+    const { temperature, summary, icon } = currently;
+    const { data: hourlyData } = hourly;
+    const { data: dailyData } = daily;
 
-    const tempEl = document.createElement("span");
-    tempEl.classList.add("weather__forecasts__temperature");
-    tempEl.innerHTML = `${Math.round(
+    // display current weather
+    const temp = document.querySelector("#temp");
+    temp.innerHTML = `${Math.round(
       temperature
     )}<span class="temperature-degree">&#176;</span>`;
-    hourlyContainer.appendChild(timeEl);
-    hourlyContainer.appendChild(iconEl);
-    hourlyContainer.appendChild(tempEl);
-    hourlyEl.appendChild(hourlyContainer);
 
-    // console.log(hourlyContainer);
+    const iconImg = document.querySelector("#icon");
+    iconImg.src = `https://darksky.net/images/weather-icons/${icon}.png`;
 
-    $(".owl-carousel").owlCarousel();
-    $(".owl-carousel").data("owl.carousel").add([hourlyContainer, 0]);
-    $(".owl-carousel").data("owl.carousel").refresh();
+    const summaryEl = document.querySelector("#summary");
+    summaryEl.innerHTML = summary;
+
+    const hourlySummary = document.querySelector("#hourly-summary");
+    hourlySummary.innerHTML = hourly.summary;
+
+    // display hourly weather
+    const hourlyEl = document.querySelector("#hourly");
+    hourlyData.forEach((hour) => {
+      const { time, icon, temperature } = hour;
+      const hourlyContainer = document.createElement("div");
+      hourlyContainer.classList.add(
+        "item",
+        "d-flex",
+        "flex-column",
+        "align-items-center"
+      );
+      const timeEl = document.createElement("p");
+      timeEl.classList.add("weather__forecasts__time");
+      if (new Date(time * 1000).getHours() === new Date().getHours()) {
+        timeEl.innerHTML = "Now";
+      } else {
+        timeEl.innerHTML = new Date(time * 1000).toLocaleTimeString("en-US", {
+          hour: "numeric",
+          minute: "numeric",
+          hour12: false,
+        });
+      }
+
+      const iconEl = document.createElement("img");
+      iconEl.classList.add("weather__forecasts__img");
+      iconEl.src = `https://darksky.net/images/weather-icons/${icon}.png`;
+
+      const tempEl = document.createElement("span");
+      tempEl.classList.add("weather__forecasts__temperature");
+      tempEl.innerHTML = `${Math.round(
+        temperature
+      )}<span class="temperature-degree">&#176;</span>`;
+      hourlyContainer.appendChild(timeEl);
+      hourlyContainer.appendChild(iconEl);
+      hourlyContainer.appendChild(tempEl);
+      hourlyEl.appendChild(hourlyContainer);
+
+      // console.log(hourlyContainer);
+
+      $(".owl-carousel").owlCarousel();
+      $(".owl-carousel").data("owl.carousel").add([hourlyContainer, 0]);
+      $(".owl-carousel").data("owl.carousel").refresh();
+    });
   });
 });
+
 // hourlyEl.innerHTML = hourlyData.map((hour) => {
 //   const { time, icon, temperature } = hour;
 // return `<div class="item d-flex flex-column align-items-center">
